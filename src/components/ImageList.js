1f5926adbf919c616af4cebf7b970284428a7daa
ajai1 <emailtoajai@gmail.com>
2019-04-26 16:57:51 +0530

    ImageGrid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,22 @@
 import React from "react";
+import "./imagelist.css";
 
 class ImageList extends React.Component {
   render() {
-    const images = this.props.images.map(image => {
-      return <img src={image.urls.thumb} />;
+    const images = this.props.images.map(({ id, description, urls }) => {
+      return (
+        <div key={id} className="">
+          <div className="image">
+            <img
+              className="ui link cards card"
+              alt={description}
+              src={urls.small}
+            />
+          </div>
+        </div>
+      );
     });
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
   }
 }
 
